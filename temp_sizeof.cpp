644f0e6150f34bf080cdf4d3d090f6b33886647e
edward mighetto <emighett@ucsc.edu>
2026-05-22 15:21:10 -0700

    remove temp_sizeof.cpp (accidentally committed)

--- temp_sizeof.cpp
+++
@@ -1,7 +0,0 @@
-#include "src/cpp/include/hodgkin_huxley/synapse_base.hpp"
-#include <iostream>
-
-int main() {
-    std::cout << "sizeof(hodgkin_huxley::SynapseBase) = " << sizeof(hodgkin_huxley::SynapseBase) << std::endl;
-    return 0;
-}
